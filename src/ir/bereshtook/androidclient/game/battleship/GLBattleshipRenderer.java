11f13264c8e7301f475382af24fc5c44b2fcb3c0
mizdler <elahi2m@yahoo.com>
2014-07-22 18:32:31 +0430

    Battleship game (test)

---
+++ src/ir/bereshtook/androidclient/game/battleship/GLBattleshipRenderer.java
@@ -0,0 +1,28 @@
+package ir.bereshtook.androidclient.game.battleship;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView.Renderer;
+
+public class GLBattleshipRenderer implements Renderer {
+
+	private Ship mShip;
+	@Override
+	public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		
+	}
+	
+	@Override
+	public void onSurfaceChanged(GL10 unused, int width, int height) {
+		GLES20.glViewport(0, 0, width, height);
+	}
+	
+	@Override
+	public void onDrawFrame(GL10 unused) {
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+	}
+
+}
